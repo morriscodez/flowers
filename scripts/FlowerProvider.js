7d619e16f027d4450b861cd30adfb6c5f9875ed7
Dylan Morris <dylanrobertmorris@gmail.com>
2021-09-17 16:03:48 -0500

    compiled flower provider component

---
+++ scripts/FlowerProvider.js
@@ -0,0 +1,13 @@
+let flowers = []
+
+export const useFlowers = () => {
+    return flowers.slice()
+}
+
+export const getFlowers = () => {
+    return fetch("http://localhost:8088/flowers")
+    .then(res => res.json())
+    .then(parsedFlowers => {
+        flowers = parsedFlowers
+    })
+}
